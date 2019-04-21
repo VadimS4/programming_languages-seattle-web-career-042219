2183ec7f6e4bb99a19cc2727a982c30b1319a312
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-21 07:05:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   languages.each do |language, attributes|
     attributes.each do |attribute, type|
       if new_hash.has_key?(lang)
-        
+        new_hash[lang]
   
   new_hash
 end
